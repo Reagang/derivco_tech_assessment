1f628ad643cc1cafc1a58607ff8a8e9d95325337
Reagan Gallant <RGallant@sportinggroup.co.uk>
2024-04-14 20:42:45 +0200

    Added SQL Stored Procedure (pr_GetOrderSummary) to return a summary of orders from the data in the Northwind database

---
+++ assessment/api/Scripts/pr_GetOrderSummary.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE pr_GetOrderSummary
+ @StartDate datetime ,
+ @EndDate datetime,
+ @CustomerID nchar(5),
+ @EmployeeID int
+AS
+SELECT 
+	CONCAT( TitleOfCourtesy,' ',FirstName,' ', LastName) AS EmployeeFullName, 
+	s.CompanyName as ShipperCompanyName, 
+	c.CompanyName as CompanyName,
+	COUNT(o.OrderID) as NumberOfOrders,
+	o.OrderDate as [Date],
+	SUM(o.Freight) as TotalFreightCost,
+	COUNT(DISTINCT od.ProductID) as NumberOfDifferentProducts,
+	SUM(os.Subtotal) as TotalOrderValue
+FROM Orders As o
+	INNER JOIN Employees As e ON o.EmployeeID = e.EmployeeID
+	INNER JOIN Customers AS c ON o.CustomerID = c.CustomerID
+	INNER JOIN [Order Details] As  od ON o.OrderID = od.OrderID
+	INNER JOIN [Order Subtotals] as os  ON o.OrderID = os.OrderID
+	INNER JOIN Shippers as s ON o.ShipVia = s.ShipperID
+WHERE   (o.OrderDate BETWEEN @StartDate AND @EndDate)
+		AND o.CustomerID = ISNULL(NULLIF(@CustomerID,''),o.CustomerID)
+		AND o.EmployeeID = ISNULL(NULLIF(@EmployeeID,''),o.EmployeeID)
+		
+GROUP BY e.TitleOfCourtesy, e.FirstName,e.LastName,
+c.CompanyName, o.OrderDate, s.CompanyName
+GO
